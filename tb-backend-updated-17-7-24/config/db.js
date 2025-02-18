7f48505ab8c6c84f820ecb392ea6166ac207119f
anm27 <94228648+anm27@users.noreply.github.com>
2025-02-18 10:55:18 +0530

    Update db.js

--- tb-backend-updated-17-7-24/config/db.js
+++ tb-backend-updated-17-7-24/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://anmishra20:LzTOB8eApbXvIqx8@cluster0.k86ipkh.mongodb.net/', {
+        await mongoose.connect('mongodb+srv://anmishra20:password@cluster0.k86ipkh.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
